d389f15c6b51ab18a3b743aa1c592f461040ba7e
Ofir Aviv <ofirsecure@gmail.com>
2023-03-16 11:48:34 +0200

    code is now working locally

--- app.py
+++ app.py
@@ -1,23 +1,47 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for
 import datetime
 
-app = Flask(__name__, static_url_path="", static_folder=".")
+
+app = Flask(__name__)
+
+chats = {}
 
 @app.route("/")
 def index():
-    return app.send_static_file("index.html")
+    return render_template("index.html")
 
 @app.route("/<room>")
 def room(room):
-    return app.send_static_file("index.html")
+    return render_template("index.html")
 
 @app.route("/chat/<room>")
+def get_chat_solo(room):
+    if room in chats:
+        chat = chats.get(room, [])
+        formatted_chat = "\n".join(f"[{msg['timestamp']}] {msg['username']}: {msg['message']}" for msg in chat)
+        return formatted_chat, {"Content-Type":"text/plain"}
+    else:
+        return "Error: Chat Not Found"
+
+@app.route("/api/chat/<room>")
 def get_chat(room):
-    return 
+    if room in chats:
+        chat = chats.get(room, [])
+        formatted_chat = "\n".join(f"[{msg['timestamp']}] {msg['username']}: {msg['message']}" for msg in chat)
+        with open("C:\Users\ofir8\Desktop\chat\rooms" + room + ".txt", "a" ) as file:
+            file.write(formatted_chat)
+        return formatted_chat, {"Content-Type":"text/plain"}
+    else:
+        return "Error: Chat Not Found"
 
 @app.route("/api/chat/<room>", methods=["POST"])
 def post_chat(room):
-    return 
+    user = request.form["username"]
+    message = request.form["msg"]
+    chats.setdefault(room, [])
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    chats[room].append({"username":user, "message":message,"timestamp":timestamp})
+    return redirect(url_for("get_chat"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
