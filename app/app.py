20850e416986c80157e49fc9d689c953c4fcfe26
Ofir Aviv <ofirsecure@gmail.com>
2023-03-16 17:12:01 +0200

    left with fixing synchronous data and formatting on /chat/<room>

--- app/app.py
+++ app/app.py
@@ -6,14 +6,12 @@ import os
 
 app = Flask(__name__)
 
-# db = mysql.connector.connect(
-#   host="db",
-#   user="root",
-#   password="password",
-#   database="access_log"
-# )
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("8.8.8.8", 80))
+db = mysql.connector.connect(
+  host="db",
+  user="root",
+  password="password",
+  database="chatLogs"
+)
 # creates a root route, by default this room name is 'general'
 @app.route("/")
 def index():
@@ -25,27 +23,37 @@ def room(room):
 
 @app.route("/chat/<room>")
 def get_chat_solo(room):
-    with open('/app/rooms/' + room + ".txt", "r") as file:
-        chat = file.read()
-    return chat, {"Content-Type":"text/plain"}
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("8.8.8.8", 80))
+    internal_ip_add = s.getsockname()[0]
+    cursor = db.cursor()
+    cursor.execute("SELECT logged, username, messages FROM chat WHERE room = %s", (room,))
+    chat = cursor.fetchall()
+    formatted_chat = [f"[{msg[0]}] {msg[1]}: {msg[2]}" for msg in chat]
+    cursor.close()
+    return os.linesep.join(formatted_chat) + '\n'
 
 @app.route("/api/chat/<room>")
 def get_chat(room):
-    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("8.8.8.8", 80))
     internal_ip_add = s.getsockname()[0]
-    if not os.path.exists('/app/rooms/' + room + ".txt"):
-        os.mknod('/app/rooms/' + room + ".txt")
-    with open('/app/rooms/' + room + ".txt", "r") as file:
-        chat = file.read() + internal_ip_add
-    return chat ,{"Content-Type":"text/plain"}
+    cursor = db.cursor()
+    cursor.execute("SELECT logged, username, messages FROM chat WHERE room = %s", (room,))
+    chat = cursor.fetchall()
+    formatted_chat = [f"[{msg[0]}] {msg[1]}: {msg[2]}" for msg in chat]
+    cursor.close()
+    return f'{internal_ip_add}\n' + "\n".join(formatted_chat)
 
 @app.route("/api/chat/<room>", methods=["POST"])
 def post_chat(room):
     user = request.form["username"]
     message = request.form["msg"]
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open('/app/rooms/' + room + ".txt", "a+") as file:
-        file.write(f'{timestamp} {user}: {message}\n')
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO chat (room, logged, username, messages) VALUES(%s, %s, %s, %s)", (room, str(timestamp), user, message))
+    db.commit()
+    cursor.close()
     return redirect(url_for("get_chat", room=room))
 
 if __name__ == "__main__":
